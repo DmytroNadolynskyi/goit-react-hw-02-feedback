17faf571dc57f94da42c990679ba5d3db2181e3c
DmytroNadolynskyi <113470234+DmytroNadolynskyi@users.noreply.github.com>
2022-12-27 23:52:53 +0200

    Update Section.styled.js

--- src/components/Section/Section.styled.js
+++ src/components/Section/Section.styled.js
@@ -4,7 +4,7 @@ export const ComSection = styled.div`
   width: 320px;
   margin: 0 auto;
   text-align: center;
-  margin-bottom: 50px;
+  margin-bottom: 20px;
 `;
 
 export const Title = styled.h2`
